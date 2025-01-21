bd3a02c3ed3bdff58e61a79b645225c3523e4101
JD17VM <jdiegodiego4@gmail.com>
2025-01-21 17:54:13 -0500

    feat(carta): carta producto

--- src/widgets/CartaProducto.jsx
+++ src/widgets/CartaProducto.jsx
@@ -2,9 +2,14 @@ import styles from '../assets/styles/estilos_carta_producto.module.scss'
 
 const CartaProducto = () => {
     return (
-        <div className={styles.carta_producto}>
-
-        </div>
+        <a className={styles.carta_producto}>
+            <div className={styles.cont_imagen_producto}>
+                <img src="https://www.kia.com/content/dam/kwcms/gt/en/images/discover-kia/voice-search/parts-80-1.jpg" alt="" />
+            </div>
+            <div className={styles.cont_nombre_producto}>
+                <p>Soporte con manguera 13mm 20m y accesorios Gardena</p>
+            </div>
+        </a>
     )
 }
 
