201a55505e3725a66b77a1b27704ec7161ca5560
JD17VM <jdiegodiego4@gmail.com>
2025-01-21 01:45:04 -0500

    feat(linea_p): añadir lista productos

--- src/Inicio.jsx
+++ src/Inicio.jsx
@@ -1,9 +1,60 @@
+import { Link } from 'react-router-dom';
 import styles from './assets/styles/estilos_inicio.module.scss'
 import imageHelper from './utils/imageHelper'
 
-//import UncontrolledExample from './widgets/HeroSlider';
-
 import SliderImgs from './widgets/SliderImgs';
+
+
+const ContenedorSeccion = (props) => {
+    let claseFondo = ""
+    if(props.color_fondo === "negro"){
+        claseFondo = styles.fondo_negro;
+    }else if(props.color_fondo === "blanco"){
+        claseFondo = styles.fondo_blanco;
+    }
+    return(
+        <div className={`${styles.contenedor_seccion} ${claseFondo}`}>
+            <h2>{props.titulo}</h2>
+            {props.children}
+        </div>
+    )
+}
+
+
+const TipoProducto = ({imagen, texto}) => {
+    return (
+        <Link 
+            className={styles.cont_tipo_producto} 
+            style={{
+                backgroundImage: `url(${imagen})`,
+            }}
+        >
+            <p>{texto}</p>
+        </Link>
+    )
+}
+
+const LineaDeProductos = () => {
+    return (
+        <ContenedorSeccion titulo="Línea de Productos" color_fondo="negro">
+            <div className={styles.contenedor_linea_productos}>
+                <TipoProducto imagen={imageHelper.ImagenDemo1} texto="Este es el texto 1"/>
+                <TipoProducto imagen={imageHelper.ImagenDemo2} texto="Este es el texto 2"/>
+                <TipoProducto imagen={imageHelper.ImagenDemo3} texto="Este es el texto 3"/>
+                <TipoProducto imagen={imageHelper.ImagenDemo4} texto="Este es el texto 4"/>
+                <TipoProducto imagen={imageHelper.ImagenDemo5} texto="Este es el texto 5"/>
+            </div>
+        </ContenedorSeccion>
+    )
+}
+
+const ProductosDestacados = () => {
+    return(
+        <ContenedorSeccion titulo="Productos Destacados" color_fondo="blanco">
+
+        </ContenedorSeccion>
+    )
+}
 
 const Inicio = () => {
 
@@ -16,6 +67,8 @@ const Inicio = () => {
     return(
         <>
             <SliderImgs images={items}/>
+            <LineaDeProductos/>
+            <ProductosDestacados/>
         </>
     )
 }
