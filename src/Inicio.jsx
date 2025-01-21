e4f69292cccf4a9d0c6ccab9db10f6b3583e0438
JD17VM <jdiegodiego4@gmail.com>
2025-01-20 19:03:34 -0500

    feat(slider): slider recibe lista de objetos para imagenes

--- src/Inicio.jsx
+++ src/Inicio.jsx
@@ -6,9 +6,16 @@ import imageHelper from './utils/imageHelper'
 import SliderImgs from './widgets/SliderImgs';
 
 const Inicio = () => {
+
+    const items = [
+        { name: 'Item 1', enlace_imagen: imageHelper.ImagenDemo1 },
+        { name: 'Item 2', enlace_imagen: imageHelper.ImagenDemo2 },
+        { name: 'Item 3', enlace_imagen: imageHelper.ImagenDemo3 }
+    ];
+
     return(
         <>
-            <SliderImgs />
+            <SliderImgs images={items}/>
         </>
     )
 }
--- src/widgets/SliderImgs.jsx
+++ src/widgets/SliderImgs.jsx
@@ -1,5 +1,4 @@
 import Carousel from 'react-bootstrap/Carousel';
-import imageHelper from '../utils/imageHelper'
 
 import styled from 'styled-components'
 
@@ -27,44 +26,22 @@ const StyledCarousel = styled(Carousel)`
 
 
 
-function SliderImgs() {
+function SliderImgs({ images }) {
   return (
     <StyledCarousel data-bs-theme="dark">
-      <Carousel.Item interval={3000}>
-        <img
-          className="d-block w-100"
-          src={imageHelper.ImagenDemo1}
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h5>First slide label</h5>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item interval={3000}>
-        <img
-          className="d-block w-100"
-          src={imageHelper.ImagenDemo2}
-          alt="Second slide"
-        />
-        <Carousel.Caption >
-          <h5>Second slide label</h5>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item interval={3000}>
-        <img
-          className="d-block w-100"
-          src={imageHelper.ImagenDemo3}
-          alt="Third slide"
-        />
-        <Carousel.Caption>
-          <h5>Third slide label</h5>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
+      {images.map((imagen, index) => (
+        <Carousel.Item interval={3000} key={index}>
+          <img
+            className="d-block w-100"
+            src={imagen.enlace_imagen}
+            alt={imagen.name}
+          />
+          {/*<Carousel.Caption>
+            <h5>First slide label</h5>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+      </Carousel.Caption>*/}
+        </Carousel.Item>
+      ))}
     </StyledCarousel>
   );
 }
