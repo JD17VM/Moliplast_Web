bfbb57228d8f36665cc73524327f6816cce35fe4
JD17VM <jdiegodiego4@gmail.com>
2025-01-22 18:08:04 -0500

    trying to deploy 2

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ const data = dataPaginas.data
 function App() {
 
   return (
-    <BrowserRouter basename="/<Moliplast_Web>">
+    <BrowserRouter basename="/Moliplast_Web">
       <Navegador data={data}/>
 
       <Routes>
